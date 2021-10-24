12b8fb49b504769b2c40ddcb5061deca47dd1ce5
Alexandre Cardoso <alexandre.cardoso@positivo.com.br>
2021-10-24 18:33:39 -0300

    Styling base.html

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     # My apps
     'learning_logs',
     'users',
+    # Third party apps
+    'bootstrap4',
     # Django app
     'django.contrib.admin',
     'django.contrib.auth',
